1a9ad7b80aadb941da1164cfac72d5610a842f06
karandin <misakarandin@gmail.com>
2022-04-12 14:22:02 +0300

    add day 8

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,8 +2,8 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
-    Airplane p1 = new Airplane("Spb", 25, 4000, 2020);
-    Airplane p2= new Airplane("Moscow", 30, 5000, 2021);
-    Airplane.compareAirplanes(p1, p2);
+        Airplane p1 = new Airplane("Spb", 25, 4000, 2020);
+        Airplane p2 = new Airplane("Moscow", 30, 5000, 2021);
+        Airplane.compareAirplanes(p1, p2);
     }
 }
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,24 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        // with StringBuilder
+        long startTime = System.currentTimeMillis();
+        StringBuilder str = new StringBuilder("");
+        for (int i = 1; i <= 20000; i++)
+            str.append(i + " ");
 
+        System.out.println(str);
+        long stopTime = System.currentTimeMillis();
+        System.out.println(stopTime - startTime);
+
+        // without StringBuilder
+        startTime = System.currentTimeMillis();
+        String s = "";
+        for (int i = 1; i <= 20000; i++)
+            s += i + " ";
+
+        System.out.println(s);
+        stopTime = System.currentTimeMillis();
+        System.out.println(stopTime - startTime);
     }
 }
