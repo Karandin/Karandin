0bc5186202efb939ffe35b9aff6c21eace8b5c53
karandin <misakarandin@gmail.com>
2022-05-01 15:36:59 +0300

    add day 17

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,17 @@
 package day17;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
-
+    String [] arr = new String[8];
+        for(int i = 0; i < 4; i++) {
+            arr[i] = ChessPiece.PAWN_WHITE.getMeaning();
+            System.out.print(arr[i] + " ");
+        }
+        for(int i = 4; i < 8; i++) {
+            arr[i] = ChessPiece.ROOK_BLACK.getMeaning();
+            System.out.print(arr[i] + " ");
+        }
     }
 }
--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -3,5 +3,16 @@ package day17;
 public class Task2 {
     public static void main(String[] args) {
 
+    ChessBoard board = new ChessBoard(new ChessPiece[][]
+            {{ChessPiece.ROOK_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK, ChessPiece.EMPTY},
+            {ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK,ChessPiece.EMPTY,ChessPiece.PAWN_BLACK},
+            {ChessPiece.PAWN_BLACK,ChessPiece.EMPTY,ChessPiece.HORSE_BLACK,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.PAWN_BLACK,ChessPiece.EMPTY},
+            {ChessPiece.QUEEN_BLACK,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.ELEPHANT_WHITE,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY},
+            {ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.ELEPHANT_BLACK,ChessPiece.PAWN_WHITE,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY},
+            {ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.ELEPHANT_WHITE, ChessPiece.PAWN_WHITE,ChessPiece.EMPTY,ChessPiece.EMPTY},
+            {ChessPiece.PAWN_WHITE,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.QUEEN_WHITE,ChessPiece.EMPTY,ChessPiece.PAWN_WHITE,ChessPiece.EMPTY,ChessPiece.PAWN_WHITE},
+            {ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE,ChessPiece.EMPTY}});
+
+    board.print();
     }
 }
