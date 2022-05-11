1cac1c9e9d3bf220a514e9247c8e24dbfb72de4f
karandin <misakarandin@gmail.com>
2022-05-11 16:08:50 +0300

    add day 19 part 1

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,40 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\misak\\IdeaProjects\\JavaMarathon2021\\JavaMarathon2021\\src\\main\\resources\\dushi.txt");
+        Scanner sc = new Scanner(file);
+        sc.useDelimiter("[.,:;()?!\"\\s–]+");
+        Map<String, Integer> map = new HashMap<>();
 
+        while (sc.hasNext()) {
+            String s = sc.next();
+            if (!map.containsKey(s))
+                map.put(s, 1);
+            else
+                map.put(s, map.get(s) + 1);
+        }
+        sc.close();
+        // map.get("Чичиков"); -> "Чичиков" 601 раз.
+        List<String> list = new ArrayList<>();
+
+        String key = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+        int maxValueInMap = (Collections.max(map.values()));
+
+        for (int i = 0; i < 99; i++) {
+            list.add(key + " = " + maxValueInMap);
+            map.remove(key, maxValueInMap);
+            key = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+            maxValueInMap = (Collections.max(map.values()));
+        }
+        System.out.println(list);
     }
 }
+
